7e3a8737486b5c428892c3161208e9e5c7270817
MRwangqi <794149142@qq.com>
2017-12-21 12:25:01 +0800

    修改加载的处理

--- app/src/main/java/com/codelang/loadinglayout/LoadingLayout.java
+++ app/src/main/java/com/codelang/loadinglayout/LoadingLayout.java
@@ -1,13 +1,14 @@
 package com.codelang.loadinglayout;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
-import android.util.LongSparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * @author wangqi
@@ -28,6 +29,21 @@ public class LoadingLayout extends ConstraintLayout {
      */
     private View emptyView;
 
+    /**
+     * 空白页面的图片
+     */
+    private int emptyResource;
+    /**
+     * 空白页面的文字
+     */
+    private String strEmptyTxt;
+
+
+    /**
+     * 判断是否显示主布局的标识
+     */
+    public boolean isContentView = false;
+
 
     public LoadingLayout(Context context) {
         this(context, null);
@@ -39,6 +55,13 @@ public class LoadingLayout extends ConstraintLayout {
 
     public LoadingLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.LoadingLayout);
+        strEmptyTxt = ta.getString(R.styleable.LoadingLayout_empty_txt);
+        emptyResource = ta.getResourceId(R.styleable.LoadingLayout_empty_img, R.drawable.empty);
+        ta.recycle();
+
+
         initView(context);
     }
 
@@ -53,31 +76,21 @@ public class LoadingLayout extends ConstraintLayout {
         /*****  加载正在加载页面  *******/
         loadingView = inflater.inflate(R.layout.loading_load, this, false);
         addView(loadingView);
-        /****  加载错误页面  *******/
+        /*****  加载错误页面  *******/
         errorView = inflater.inflate(R.layout.loading_error, this, false);
         addView(errorView);
         /*****  加载空页面  *******/
         emptyView = inflater.inflate(R.layout.loading_empty, this, false);
         addView(emptyView);
 
-        loadingView.setVisibility(View.GONE);
-        errorView.setVisibility(View.GONE);
-        emptyView.setVisibility(View.GONE);
-
-//        errorView.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                iLoadingListener.error();
-//                showLoadingView();
-//            }
-//        });
-
+        ImageView emptyImg = emptyView.findViewById(R.id.empty_img);
+        TextView emptyTxt = emptyView.findViewById(R.id.empty_txt);
+        emptyImg.setImageResource(emptyResource);
+        emptyTxt.setText(strEmptyTxt);
 
     }
 
-    /**
-     * 显示主布局
-     */
+
     public void showContentView() {
         removeView(errorView);
         removeView(emptyView);
@@ -86,12 +99,12 @@ public class LoadingLayout extends ConstraintLayout {
             View view = getChildAt(i);
             view.setVisibility(View.VISIBLE);
         }
+        isContentView = true;
     }
 
-    /**
-     * 显示证咋加载错误布局
-     */
+
     public void showErrorView() {
+        if (isContentView) return;
         for (int i = 0; i < getChildCount(); i++) {
             View view = getChildAt(i);
             view.setVisibility(View.GONE);
@@ -99,10 +112,9 @@ public class LoadingLayout extends ConstraintLayout {
         errorView.setVisibility(View.VISIBLE);
     }
 
-    /**
-     * 显示正在加载布局
-     */
+
     public void showLoadingView() {
+        if (isContentView) return;
         for (int i = 0; i < getChildCount(); i++) {
             View view = getChildAt(i);
             view.setVisibility(View.GONE);
@@ -110,21 +122,13 @@ public class LoadingLayout extends ConstraintLayout {
         loadingView.setVisibility(View.VISIBLE);
     }
 
-    /**
-     * 显示空布局
-     */
     public void showEmptyView() {
+        if (isContentView) return;
         for (int i = 0; i < getChildCount(); i++) {
             View view = getChildAt(i);
             view.setVisibility(View.GONE);
         }
         emptyView.setVisibility(View.VISIBLE);
     }
-
-//    ILoadingListener iLoadingListener;
-//
-//    public void setLoadErrorListener(ILoadingListener iLoadingListener) {
-//        this.iLoadingListener = iLoadingListener;
-//    }
 
 }
